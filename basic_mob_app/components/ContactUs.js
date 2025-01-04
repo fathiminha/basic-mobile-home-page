bb5ade7952080baa6eb0b7f422ac7d024b44cc46
Fathima Minha <minhasulaikar@gmail.com>
2025-01-05 01:45:26 +0530

    create some text inputs

--- basic_mob_app/components/ContactUs.js
+++ basic_mob_app/components/ContactUs.js
@@ -1,7 +1,5 @@
-import { StyleSheet } from "react-native"
-import { View } from "react-native-web"
-import { Divider,Button, Text} from 'react-native-paper';
-
+import { StyleSheet,View } from "react-native";
+import { Divider,Button, Text, TextInput} from 'react-native-paper';
 
 export default function ContactUs(){
     return(
@@ -11,7 +9,18 @@ export default function ContactUs(){
                 <Divider />
             </View>
             <View style={styles.body}>
-                
+                <View>
+                  <TextInput label="Name" mode="outlined" />
+                </View>
+                <View>
+                 <TextInput label="Email" mode="outlined" keyboardType="email-address" />
+                </View> 
+                <View>
+                 <TextInput label="Phone Number" mode="outlined" keyboardType="phone-pad" />
+                </View> 
+                <View>
+                 <TextInput label="Message" mode="outlined" multiline numberOfLines={5} />
+                </View> 
             </View>
             <View style={styles.footer}>
                 <Text> 2024 </Text>
@@ -24,16 +33,20 @@ const styles = StyleSheet.create({
     header:{
         flex:3,
         width:"100%",
-        alignItems:"center"
+        alignItems:"center",
+        padding:10,
+        marginBottom:10
     },
     body:{
         flex:5,
-        width:"100%",
-        alignItems:"center"
+        width:"100%"
     },
     footer:{
         flex:2,
-        width:"100%",
-        alignItems:"center"
+        width:"100%"
+    },
+    input:{
+        padding:8,
+        marginBottom:7
     }
 })
