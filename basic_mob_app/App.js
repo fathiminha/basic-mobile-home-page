805808b7933e8a73728dc990d26123e342b35d89
Fathima Minha <minhasulaikar@gmail.com>
2025-01-05 00:06:52 +0530

    updated app

--- basic_mob_app/App.js
+++ basic_mob_app/App.js
@@ -1,15 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-import { PaperProvider, Text} from 'react-native-paper';
+import { StyleSheet, View, ScrollView } from 'react-native';
+import { PaperProvider} from 'react-native-paper';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import Home from './components/Home';
 
 export default function App() {
   return (
     <PaperProvider>
-      <View style={styles.container}>
-        <Home />
-        <StatusBar style="auto" />
-      </View>
+      <SafeAreaView>
+        <ScrollView>
+          <View style={styles.container}>
+            <Home />
+            <StatusBar style="auto" />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
     </PaperProvider>
     
   );
